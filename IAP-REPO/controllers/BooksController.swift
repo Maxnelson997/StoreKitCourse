111ca509ff22d9ce5400ac3cab0a5a13f06f02dd
Max <maxnelson997@gmail.com>
2019-05-30 13:51:38 -0600

    books controller tableview setup

--- IAP-REPO/controllers/BooksController.swift
+++ IAP-REPO/controllers/BooksController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class BooksController: UIViewController {
+    
+    fileprivate let tb: UITableView = {
+        let tb = UITableView()
+        tb.translatesAutoresizingMaskIntoConstraints = false
+        tb.separatorStyle = .none
+        tb.allowsSelection = false
+        tb.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tb
+    }()
+    
     override func viewDidLoad() {
         self.navigationItem.title = "maxreads"
         view.backgroundColor = .white
     
         setupAddToCartButton()
+        setupTableView()
     }
     
     fileprivate func setupAddToCartButton() {
@@ -26,7 +37,35 @@ class BooksController: UIViewController {
         cart.addTarget(self, action: #selector(goToCart), for: .touchUpInside)
     }
     
+    fileprivate func setupTableView() {
+        view.addSubview(tb)
+        tb.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        tb.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        tb.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        tb.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        tb.dataSource = self
+        tb.delegate = self
+    }
+    
     @objc func goToCart() {
         self.navigationController?.pushViewController(CartController(), animated: true)
     }
+}
+
+extension BooksController: UITableViewDelegate {
+    
+}
+
+extension BooksController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "product title and IAP price you will charge the user."
+        return cell
+    }
+    
+    
 }
