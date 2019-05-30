28afcea7873a383d364103a6972b40a99bd41549
Max <maxnelson997@gmail.com>
2019-05-30 14:27:29 -0600

    setting up the store credit labels

--- IAP-REPO/controllers/CartController.swift
+++ IAP-REPO/controllers/CartController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class CartController: UIViewController {
     
+    fileprivate let totalLabel: UILabel = UILabel()
+    fileprivate let balanceLabel: UILabel = UILabel()
+    
     fileprivate let tb: UITableView = {
         let tb = UITableView()
         tb.translatesAutoresizingMaskIntoConstraints = false
@@ -19,13 +22,13 @@ class CartController: UIViewController {
         return tb
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "cart"
         view.backgroundColor = .white
         
         setupTableView()
+        setupCartButtons()
     }
     
     fileprivate func setupTableView() {
@@ -33,10 +36,39 @@ class CartController: UIViewController {
         tb.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
         tb.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         tb.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
-        tb.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        tb.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -300).isActive = true
         tb.dataSource = self
         tb.delegate = self
     }
+    
+    fileprivate func setupCartButtons() {
+        totalLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        totalLabel.text = "Total: 0 credits"
+        
+        balanceLabel.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        balanceLabel.text = "Balance: 0 credits"
+        
+        let line = UIView()
+        line.backgroundColor = .black
+        
+        let line1 = UIView()
+        line1.backgroundColor = .black
+        
+        
+        let stack = UIStackView(arrangedSubviews: [totalLabel, line, balanceLabel, line1])
+        stack.axis = .vertical
+        stack.spacing = 30
+        view.addSubview(stack)
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        
+        stack.topAnchor.constraint(equalTo: tb.bottomAnchor).isActive = true
+        stack.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        line.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        line1.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
     
 }
 
