b61b5667223aa3dbbde03ebbf401a0df9aeee0bf
Max <maxnelson997@gmail.com>
2019-05-30 13:23:21 -0600

    init commit and first video

---
+++ IAP-REPO/BooksController.swift
@@ -0,0 +1,16 @@
+//
+//  BooksController.swift
+//  IAP-REPO
+//
+//  Created by Max Nelson on 5/30/19.
+//  Copyright © 2019 Maxcodes. All rights reserved.
+//
+
+import UIKit
+
+class BooksController: UIViewController {
+    override func viewDidLoad() {
+        self.navigationItem.title = "maxreads"
+        view.backgroundColor = .white
+    }
+}
---
+++ IAP-REPO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAP-REPO
+//
+//  Created by Max Nelson on 5/30/19.
+//  Copyright © 2019 Maxcodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
