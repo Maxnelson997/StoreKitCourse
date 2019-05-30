d1c4fb663dd8a0e23cfbfa2770d74124c9a4c783
Max <maxnelson997@gmail.com>
2019-05-30 14:20:37 -0600

    Setup the CartController TableView

--- IAP-REPO/controllers/CartController.swift
+++ IAP-REPO/controllers/CartController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class CartController: UIViewController {
+    
+    fileprivate let tb: UITableView = {
+        let tb = UITableView()
+        tb.translatesAutoresizingMaskIntoConstraints = false
+        tb.separatorStyle = .none
+        tb.allowsSelection = false
+        tb.register(MCBookViewCell.self, forCellReuseIdentifier: "cell")
+        return tb
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "cart"
         view.backgroundColor = .white
+        
+        setupTableView()
+    }
+    
+    fileprivate func setupTableView() {
+        view.addSubview(tb)
+        tb.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        tb.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        tb.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        tb.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
+        tb.dataSource = self
+        tb.delegate = self
+    }
+    
+}
+
+extension CartController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
 
+extension CartController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MCBookViewCell
+        cell.book = books[indexPath.row]
+        return cell
+    }
+    
+    
+}
